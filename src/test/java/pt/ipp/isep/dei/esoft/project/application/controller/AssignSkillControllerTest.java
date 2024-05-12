bec436319ea7b41348798b33290e828e5dd31402
Tomás Moreira <78792066+tmas282@users.noreply.github.com>
2024-05-12 23:33:09 +0100

    starting test of controller AssignSkillControllerTest

--- src/test/java/pt/ipp/isep/dei/esoft/project/application/controller/AssignSkillControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/application/controller/AssignSkillControllerTest.java
@@ -34,4 +34,31 @@ class AssignSkillControllerTest {
         assignSkillController.setCollaboratorSelected(c1);
         assignSkillController.addSkills();
     }
+    @Test
+    void assignTheSameSkillToCollaborator(){
+        Collaborator c1 = new Collaborator("tomaspintomoreira28@gmail.com");
+        Collaborator c2 = new Collaborator("1231087@isep.ipp.pt.com");
+
+        Repositories.getInstance().getCollaboratorsRepository().add(c1);
+        Repositories.getInstance().getCollaboratorsRepository().add(c2);
+
+        Skill s1 = new Skill("Dedicação");
+        Skill s2 = new Skill("Carta Barco");
+
+        Repositories.getInstance().getSkillsRepository().add(s1);
+        Repositories.getInstance().getSkillsRepository().add(s2);
+
+        c1.addSkill(s1);
+        Collaborator c3 = c1.clone();
+
+        List<Skill> skillList = new ArrayList<>();
+        skillList.add(s1);
+
+        AssignSkillController assignSkillController = new AssignSkillController();
+        assignSkillController.storeSkills(skillList);
+        assignSkillController.setCollaboratorSelected(c1);
+        assignSkillController.addSkills();
+
+        assertEquals(c3.getSkills(), c1.getSkills());
+    }
 }
