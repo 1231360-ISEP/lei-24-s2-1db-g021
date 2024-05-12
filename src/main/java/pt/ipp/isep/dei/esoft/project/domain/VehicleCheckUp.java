eeec9262a309da003c7d026c48afbba70f26a859
Tomás Moreira <78792066+tmas282@users.noreply.github.com>
2024-05-12 18:34:30 +0100

    Working on VehicleCheckUp class

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/VehicleCheckUp.java
@@ -0,0 +1,10 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+import java.util.Date;
+
+public class VehicleCheckUp {
+    private Date date;
+    private Address place;
+    private float kms;
+
+}
