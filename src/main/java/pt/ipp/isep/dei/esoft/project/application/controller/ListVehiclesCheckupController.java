5b63987ab39e5bef5c7584c153ce42aca6a3390a
Tomás Moreira <78792066+tmas282@users.noreply.github.com>
2024-05-12 23:56:14 +0100

    ListVehiclesCheckupController changed

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ListVehiclesCheckupController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ListVehiclesCheckupController.java
@@ -1,7 +1,10 @@
 package pt.ipp.isep.dei.esoft.project.application.controller;
 
+import pt.ipp.isep.dei.esoft.project.domain.Vehicle;
 import pt.ipp.isep.dei.esoft.project.repository.Repositories;
 import pt.ipp.isep.dei.esoft.project.repository.VehiclesRepository;
+
+import java.util.List;
 
 public class ListVehiclesCheckupController {
     private VehiclesRepository vehiclesRepository;
@@ -19,5 +22,30 @@ public class ListVehiclesCheckupController {
             vehiclesRepository = repositories.getVehiclesRepository();
         }
         return vehiclesRepository;
+    }
+
+    public void getVehiclesNeedingCheckup(){
+        List<Vehicle> vehicles = vehiclesRepository.getVehiclesList();
+        for (Vehicle vehicle:
+             vehicles) {
+
+        }
+    }
+
+    private boolean isVehicleNeedingOrAlmostCheckup(Vehicle vehicle){
+        return true;
+    }
+    private boolean didVehExceededCheckup(Vehicle vehicle){
+        double maintenance = vehicle.getMaintenance();
+        double currentKm = vehicle.getCurrentKm();
+        return false;
+    }
+    private boolean isVehicleCloseToExceedCheckup(Vehicle vehicle){
+        double maintenance = vehicle.getMaintenance();
+        double currentKm = vehicle.getCurrentKm();
+        double maintenanceSum = 0;
+        while(maintenanceSum <= currentKm){
+            maintenanceSum += maintenance;
+        }
     }
 }
