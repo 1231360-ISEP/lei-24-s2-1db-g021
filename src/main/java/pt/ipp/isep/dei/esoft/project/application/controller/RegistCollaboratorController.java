9f2794fac437ce2a3f5227aaa83d9fa983e0af9c
Bernardo Cardoso <1231070@isep.ipp.pt>
2024-05-12 23:55:21 +0100

    Updating RegistCollaboratorController

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegistCollaboratorController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegistCollaboratorController.java
@@ -1,108 +1,69 @@
 package pt.ipp.isep.dei.esoft.project.application.controller;
 
 import pt.ipp.isep.dei.esoft.project.domain.Collaborator;
-import pt.ipp.isep.dei.esoft.project.domain.Task;
-import pt.ipp.isep.dei.esoft.project.domain.TaskCategory;
-import pt.ipp.isep.dei.esoft.project.repository.AuthenticationRepository;
-import pt.ipp.isep.dei.esoft.project.repository.Repositories;
-import pt.ipp.isep.dei.esoft.project.repository.TaskCategoryRepository;
-import pt.ipp.isep.dei.esoft.project.repository.TasksRepository;
-import pt.isep.lei.esoft.auth.domain.model.Email;
+import pt.ipp.isep.dei.esoft.project.domain.Job;
+import pt.ipp.isep.dei.esoft.project.repository.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class RegistCollaboratorController {
+    private final Repositories repositories = Repositories.getInstance();
 
-    private TaskCategoryRepository taskCategoryRepository;
-    private AuthenticationRepository authenticationRepository;
-    private TasksRepository tasksRepository;
-
-
-    //Repository instances are obtained from the Repositories class
+    private CollaboratorsRepository collaboratorsRepository;
+    private JobsRepository jobsRepository;
+    private Job job;
+    private List<Job> jobSelected;
     public RegistCollaboratorController() {
-        getTaskCategoryRepository();
-        getAuthenticationRepository();
-        getTaskRepository();
+        getJobsRepository();
+        getCollaboratorsRepository();
     }
-
-    //Allows receiving the repositories as parameters for testing purposes
-    public RegistCollaboratorController(TaskCategoryRepository taskCategoryRepository,
-                                        AuthenticationRepository authenticationRepository,
-                                        TasksRepository tasksRepository) {
-        this.taskCategoryRepository = taskCategoryRepository;
-        this.authenticationRepository = authenticationRepository;
-        this.tasksRepository = tasksRepository;
+    public RegistCollaboratorController(CollaboratorsRepository collaboratorsRepository, JobsRepository jobsRepository) {
+        this.collaboratorsRepository = collaboratorsRepository;
+        this.jobsRepository = jobsRepository;
     }
+    private JobsRepository getJobsRepository() {
+        if (jobsRepository == null) {
+            Repositories repositories = Repositories.getInstance();
 
-    private TaskCategoryRepository getTaskCategoryRepository() {
-        if (taskCategoryRepository == null) {
+            jobsRepository = repositories.getJobsRepository();
+        }
+        return jobsRepository;
+    }
+    private CollaboratorsRepository getCollaboratorsRepository(){
+        if (collaboratorsRepository == null) {
             Repositories repositories = Repositories.getInstance();
 
             //Get the TaskCategoryRepository
-            taskCategoryRepository = repositories.getTaskCategoryRepository();
+            collaboratorsRepository = repositories.getCollaboratorsRepository();
         }
-        return taskCategoryRepository;
+        return collaboratorsRepository;
     }
 
-    private AuthenticationRepository getAuthenticationRepository() {
-        if (authenticationRepository == null) {
-            Repositories repositories = Repositories.getInstance();
-
-            //Get the AuthenticationRepository
-            authenticationRepository = repositories.getAuthenticationRepository();
+    public List<Collaborator> getCollaborators(){
+        List<Collaborator> collaboratorsList = collaboratorsRepository.getCollaboratorsList();
+        return collaboratorsList;
+    }
+    public void setJobSelected(Job job){
+        this.job = job;
+    }
+    public Optional<Job> createJob(String name) {
+        Optional<Job> newJob;
+        try {
+            newJob = jobsRepository.add(new Job(name));
+        } catch (IllegalArgumentException e) {
+            return Optional.empty();
         }
-        return authenticationRepository;
+        return  newJob;
     }
-
-    private TasksRepository getTaskRepository() {
-        if (tasksRepository == null) {
-            Repositories repositories = Repositories.getInstance();
-
-            tasksRepository = repositories.getTaskRepository();
+    public void storeJob(List<Skill> skillsToAssign){
+        this.skillsToAssign = skillsToAssign;
+    }
+    public void addSkills(){
+        for (Skill skill:
+                skillsToAssign) {
+            collaboratorSelected.addSkill(skill);
         }
-
-        return tasksRepository;
-    }
-
-    public Optional<Task> createTask(String reference, String description, String informalDescription,
-                                     String technicalDescription, int duration, double cost,
-                                     String taskCategoryDescription) {
-
-        TaskCategory taskCategory = getTaskCategoryByDescription(taskCategoryDescription);
-
-        Collaborator collaborator = getCollaboratorFromSession();
-
-        Optional<Task> newTask = Optional.empty();
-
-        newTask = getTaskRepository().add(new Task(
-                reference, description, informalDescription,
-                technicalDescription, duration, cost,
-                taskCategory, collaborator
-        ));
-
-        return newTask;
-    }
-
-    private Collaborator getCollaboratorFromSession() {
-        Email email = getAuthenticationRepository().getCurrentUserSession().getUserId();
-        return new Collaborator(email.getEmail());
-    }
-
-
-    private TaskCategory getTaskCategoryByDescription(String taskCategoryDescription) {
-        TaskCategoryRepository taskCategoryRepository = getTaskCategoryRepository();
-
-        //Get the TaskCategory by its description
-        TaskCategory taskCategoryByDescription =
-                getTaskCategoryRepository().getTaskCategoryByDescription(taskCategoryDescription);
-        return taskCategoryByDescription;
-
-    }
-
-    //return the list of task categories
-    public List<TaskCategory> getTaskCategories() {
-        TaskCategoryRepository taskCategoryRepository = getTaskCategoryRepository();
-        return taskCategoryRepository.getTaskCategories();
     }
 }
