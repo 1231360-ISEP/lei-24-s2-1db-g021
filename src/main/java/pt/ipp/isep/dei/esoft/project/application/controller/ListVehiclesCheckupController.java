b825e16e6c22488379c6794a4555a092d1be78fd
Tomás Moreira <78792066+tmas282@users.noreply.github.com>
2024-05-14 11:17:14 +0100

    Updating ListVehiclesCheckupController

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ListVehiclesCheckupController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ListVehiclesCheckupController.java
@@ -4,6 +4,7 @@ import pt.ipp.isep.dei.esoft.project.domain.Vehicle;
 import pt.ipp.isep.dei.esoft.project.repository.Repositories;
 import pt.ipp.isep.dei.esoft.project.repository.VehiclesRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListVehiclesCheckupController {
@@ -24,21 +25,30 @@ public class ListVehiclesCheckupController {
         return vehiclesRepository;
     }
 
-    public void getVehiclesNeedingCheckup(){
+    public List<Vehicle> getVehiclesNeedingCheckup(){
         List<Vehicle> vehicles = vehiclesRepository.getVehiclesList();
+        List<Vehicle> vehiclesNeedingIt = new ArrayList<Vehicle>();
         for (Vehicle vehicle:
              vehicles) {
-
+                if(isVehicleCloseToExceedCheckup(vehicle)){
+                    vehiclesNeedingIt.add(vehicle);
+                }
         }
+        return vehiclesNeedingIt;
     }
 
     private boolean isVehicleNeedingOrAlmostCheckup(Vehicle vehicle){
-        return true;
+        return didVehExceededCheckup(vehicle) || isVehicleCloseToExceedCheckup(vehicle);
     }
     private boolean didVehExceededCheckup(Vehicle vehicle){
         double maintenance = vehicle.getMaintenance();
         double currentKm = vehicle.getCurrentKm();
-        return false;
+        double maintenanceGoal = 0;
+        while(maintenanceGoal <= currentKm){
+            maintenanceGoal += maintenance;
+        }
+        maintenanceGoal -= maintenance;
+        return currentKm > maintenance;
     }
     private boolean isVehicleCloseToExceedCheckup(Vehicle vehicle){
         double maintenance = vehicle.getMaintenance();
