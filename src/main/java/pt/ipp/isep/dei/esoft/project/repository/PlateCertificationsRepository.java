780666818108ca1b65883ac321c9f3d7cc8cc4f2
João Jorge <1231080@isep.ipp.pt>
2024-05-14 21:47:58 +0100

    adding more javaDoc to some Classes (continuation)

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/PlateCertificationsRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/PlateCertificationsRepository.java
@@ -1,6 +1,5 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
-import pt.ipp.isep.dei.esoft.project.domain.Job;
 import pt.ipp.isep.dei.esoft.project.domain.PlateCertification;
 
 import java.util.ArrayList;
@@ -10,10 +9,19 @@ import java.util.Optional;
 public class PlateCertificationsRepository {
     private final List<PlateCertification> plateCertificationsList;
 
+    /**
+     * Constructs a new PlateCertificationsRepository object.
+     */
     public PlateCertificationsRepository() {
         plateCertificationsList = new ArrayList<>();
     }
 
+    /**
+     * Adds a new PlateCertification to the repository.
+     *
+     * @param plateCertification The PlateCertification object to be added.
+     * @return An Optional containing the newly added PlateCertification, or empty if the operation fails.
+     */
     public Optional<PlateCertification> add(PlateCertification plateCertification) {
 
         Optional<PlateCertification> newPlateCertification = Optional.empty();
@@ -36,15 +44,21 @@ public class PlateCertificationsRepository {
     }
 
     /**
-     * This method returns a defensive (immutable) copy of the list of plate certifications.
+     * Retrieves a defensive (immutable) copy of the list of plate certifications.
      *
      * @return The list of plate certifications.
      */
     public List<PlateCertification> getPlateCertificationsList() {
-        //This is a defensive copy, so that the repository cannot be modified from the outside.
+        // This is a defensive copy, so that the repository cannot be modified from the outside.
         return List.copyOf(plateCertificationsList);
     }
 
+    /**
+     * Removes a PlateCertification from the repository.
+     *
+     * @param plateCertification The PlateCertification object to be removed.
+     * @return True if the PlateCertification was successfully removed, false otherwise.
+     */
     public boolean remove(PlateCertification plateCertification) {
         return plateCertificationsList.remove(plateCertification);
     }
