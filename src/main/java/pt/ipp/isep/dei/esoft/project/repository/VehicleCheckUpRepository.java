dfa3828dafb1417cd59d71a496234798d8d02ba2
Tomás Moreira <78792066+tmas282@users.noreply.github.com>
2024-05-14 10:27:25 +0100

    Created a VehicleCheckUp repository called VehicleCheckUpRepository

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/VehicleCheckUpRepository.java
@@ -0,0 +1,50 @@
+package pt.ipp.isep.dei.esoft.project.repository;
+
+import pt.ipp.isep.dei.esoft.project.domain.VehicleCheckUp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class VehicleCheckUpRepository {
+    private final List<VehicleCheckUp> vehiclesCheckUpList;
+
+    public VehicleCheckUpRepository() {
+        vehiclesCheckUpList = new ArrayList<>();
+    }
+
+    public Optional<VehicleCheckUp> add(VehicleCheckUp vehicleCheckUp) {
+
+        Optional<VehicleCheckUp> newVehicleCheckUp = Optional.empty();
+        boolean operationSuccess = false;
+
+        if (validateVehicleCheckUp(vehicleCheckUp)) {
+            newVehicleCheckUp = Optional.of(vehicleCheckUp.clone());
+            operationSuccess = vehiclesCheckUpList.add(vehicleCheckUp.get());
+        }
+
+        if (!operationSuccess) {
+            newVehicleCheckUp = Optional.empty();
+        }
+
+        return newVehicleCheckUp;
+    }
+
+    private boolean validateVehicleCheckUp(VehicleCheckUp vehicle) {
+        return !vehiclesCheckUpList.contains(vehicle);
+    }
+
+    /**
+     * This method returns a defensive (immutable) copy of the list of vehicles check-ups.
+     *
+     * @return The list of vehicles check-ups.
+     */
+    public List<VehicleCheckUp> getVehiclesList() {
+        //This is a defensive copy, so that the repository cannot be modified from the outside.
+        return List.copyOf(vehiclesCheckUpList);
+    }
+
+    public boolean remove(VehicleCheckUp vehicleCheckUp) {
+        return vehiclesCheckUpList.remove(vehicleCheckUp);
+    }
+}
